a7446adf52137db76b8c2225c88d32182962f37b
Danielle Van Allen <77242629+felcandy@users.noreply.github.com>
2022-12-10 14:54:24 -0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
+/*
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+*/
+
+module.exports = {
+  pwa: {
+      workboxOptions: {
+          exclude: [/\.map$/, /web\.config$/],
+      },
+  },
+};
